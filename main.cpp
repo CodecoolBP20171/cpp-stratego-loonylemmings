062a6dab2212b9635528be99069a1f16eeec437d
Péter Biró <biro.peter.hu@gmail.com>
2017-11-08 15:26:35 +0100

    trying SDL

--- main.cpp
+++ main.cpp
@@ -1,6 +1,110 @@
-#include <iostream>
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 602;
+const int SCREEN_HEIGHT = 602;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+SDL_Surface* bomg_pic = NULL;
+
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "St(a)r(atego) Wars", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gHelloWorld = SDL_LoadBMP( "../Pics/Base_Board.bmp" );
+
+    if( gHelloWorld == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", "Pix/empire_alliance.bmp", SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+        //Start up SDL and create window
+        if( !init() )
+        {
+            printf( "Failed to initialize!\n" );
+        }
+        else
+        {
+            //Load media
+            if( !loadMedia() )
+            {
+                printf( "Failed to load media!\n" );
+            }
+            else
+            {
+                //Apply the image
+                SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+
+                //Update the surface
+                SDL_UpdateWindowSurface( gWindow );
+                //Wait two seconds
+                SDL_Delay( 12000 );
+            }
+        }
+
+        //Free resources and close SDL
+        close();
+
+        return 0;
 }
