23cfc2f63a3b322c993c93f7dd7db4eb57a628d7
ReginaCsaszar <regina.csaszar@gmail.com>
2017-11-10 19:58:46 +0100

    get rid of viewports and use simply rectangles for positioning pictures, implement a card flip at click sample, compress and refactor code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
 
-//Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
@@ -11,229 +8,168 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-/*
-//Starts up SDL and creates window
-bool init();
 
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//Loads individual image as texture
-SDL_Texture* loadTexture( std::string path );
-*/
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
+SDL_Window* gWindow = nullptr;
+SDL_Renderer* gRenderer = nullptr;
 
 //Current displayed texture
-SDL_Texture* board = NULL;
-SDL_Texture* bomb = NULL;
+SDL_Texture* board = nullptr;
+SDL_Texture* bomb = nullptr;
+SDL_Texture* bckgr = nullptr;
 
-bool init()
-{
-    //Initialization flag
-    bool success = true;
-
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
+bool init() {
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
         printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-        success = false;
+        return false;
     }
-    else
-    {
-        //Set texture filtering to linear
-        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-        {
-            printf( "Warning: Linear texture filtering not enabled!" );
-        }
-
-        //Create window
-        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( gWindow == NULL )
-        {
-            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-            success = false;
-        }
-        else
-        {
-            //Create renderer for window
-            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-            if( gRenderer == NULL )
-            {
-                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-                success = false;
-            }
-            else
-            {
-                //Initialize renderer color
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-                //Initialize PNG loading
-                int imgFlags = IMG_INIT_PNG;
-                if( !( IMG_Init( imgFlags ) & imgFlags ) )
-                {
-                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-                    success = false;
-                }
-            }
-        }
+    //Set texture filtering to linear
+    if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) ) {
+        printf( "Warning: Linear texture filtering not enabled!" );
     }
-
-    return success;
+    //Create window
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( !gWindow ) {
+        printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+        return false;
+    }
+    //Create renderer for window
+    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+    if( !gRenderer ) {
+        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+        return false;
+    }
+    //Initialize renderer color
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    //Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
+        printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+        return false;
+    }
+    return true;
 }
 
-SDL_Texture* loadTexture( std::string path )
-{
+SDL_Texture* loadTexture( std::string path ) {
     //The final texture
-    SDL_Texture* newTexture = NULL;
+    SDL_Texture* newTexture = nullptr;
 
     //Load image at specified path
     SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-    if( loadedSurface == NULL )
-    {
+    if(!loadedSurface) {
         printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-    }
-    else
-    {
+    } else {
         //Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-        if( newTexture == NULL )
-        {
+        if(!newTexture) {
             printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
         }
-
         //Get rid of old loaded surface
-        SDL_FreeSurface( loadedSurface );
+        SDL_FreeSurface(loadedSurface);
     }
-
     return newTexture;
 }
 
-bool loadMedia()
-{
-    //Loading success flag
-    bool success = true;
-
-    //Load PNG texture
+bool loadMedia() {
     board = loadTexture( "../Pics/Base_Board.png" );
     bomb = loadTexture( "../Pics/Selection_120.png" );
-    if( board == NULL||bomb == NULL )
-    {
+    bckgr = loadTexture("../Pics/BGR.jpg");
+    if(!board) {
         printf( "Failed to load texture image!\n" );
-        success = false;
     }
-
-    return success;
+    return true;
 }
 
 void close()
 {
     //Free loaded image
     SDL_DestroyTexture( board );
-    board = NULL;
+    board = nullptr;
+    bomb = nullptr;
+    bckgr = nullptr;
 
     //Destroy window
     SDL_DestroyRenderer( gRenderer );
     SDL_DestroyWindow( gWindow );
-    gWindow = NULL;
-    gRenderer = NULL;
+    gWindow = nullptr;
+    gRenderer = nullptr;
 
     //Quit SDL subsystems
     IMG_Quit();
     SDL_Quit();
 }
 
-
-
-int main( int argc, char* args[] )
-{
+int main( int argc, char* args[] ) {
     //Start up SDL and create window
-    if( !init() )
-    {
+    if( !init() ) {
         printf( "Failed to initialize!\n" );
-    }
-    else
-    {
+    } else {
         //Load media
-        if( !loadMedia() )
-        {
+        if( !loadMedia() ) {
             printf( "Failed to load media!\n" );
-        }
-        else
-        {
-            //Main loop flag
+        } else {
+            //Clear screen
+            SDL_RenderClear( gRenderer );
+
+            //set bg coordinates and render bg into it
+            SDL_Rect bg = { 0, 0, 600, 600 };
+            SDL_RenderCopy( gRenderer, board, nullptr, &bg );
+
+            //same for bomb
+            SDL_Rect ins = {130, 130, 50, 50};
+            SDL_RenderCopy( gRenderer, bomb, nullptr, &ins );
+
+            //show
+            SDL_RenderPresent( gRenderer );
+
             bool quit = false;
+            bool flipped = true;
 
             //Event handler
             SDL_Event e;
 
             //While application is running
-            while( !quit )
-            {
+            while( !quit ) {
                 //Handle events on queue
-                while( SDL_PollEvent( &e ) != 0 )
-                {
+                while( SDL_PollEvent( &e ) != 0 ) {
+
+                    //if clicked (mouse button released)
+                    if( e.type == SDL_MOUSEBUTTONUP) {
+
+                        //ask for mouse button coordinates
+                        int x, y;
+                        SDL_GetMouseState( &x, &y );
+
+                        //check if clicked on bomb
+                        if (x<180 && x>130 && y<180 && y>130) {
+
+                            //kill'em all and render bg in its place
+                            SDL_RenderClear( gRenderer );
+                            SDL_RenderCopy( gRenderer, board, nullptr, &bg );
+
+
+                            //render bomb or card_bg depend on flipped state
+                            if (flipped) {
+                                SDL_RenderCopy( gRenderer, bckgr, nullptr, &ins );
+                            } else {
+                                SDL_RenderCopy( gRenderer, bomb, nullptr, &ins );
+                            }
+
+                            flipped = !flipped;
+                            //show
+                            SDL_RenderPresent( gRenderer );
+                        }
+                    }
+
                     //User requests quit
-                    if( e.type == SDL_QUIT )
-                    {
+                    if( e.type == SDL_QUIT ) {
                         quit = true;
                     }
                 }
-                //Clear screen
-                SDL_RenderClear( gRenderer );
-
-                //full viewport
-                SDL_Rect fullViewport;
-                fullViewport.x = 0;
-                fullViewport.y = 0;
-                fullViewport.w = 602;
-                fullViewport.h = 602;
-                SDL_RenderSetViewport( gRenderer, &fullViewport );
-
-                //Render texture to screen
-                SDL_RenderCopy( gRenderer, board, NULL, NULL );
-
-                //inside viewport
-                SDL_Rect insideViewport;
-                insideViewport.x = 130;
-                insideViewport.y = 130;
-                insideViewport.w = 50;
-                insideViewport.h = 50;
-                SDL_RenderSetViewport( gRenderer, &insideViewport );
-
-                //Render texture to screen
-                SDL_RenderCopy( gRenderer, bomb, NULL, NULL );
-
-                //Update screen
-                SDL_RenderPresent( gRenderer );
             }
-
-            //inside viewport
-            SDL_Rect insideViewport;
-            insideViewport.x = 230;
-            insideViewport.y = 230;
-            insideViewport.w = 50;
-            insideViewport.h = 50;
-            SDL_RenderSetViewport( gRenderer, &insideViewport );
-
-            //Render texture to screen
-            SDL_RenderCopy( gRenderer, bomb, NULL, NULL );
-
-            //Update screen
-            SDL_RenderPresent( gRenderer );
-            SDL_Delay(2000);
+            SDL_Delay(500);
         }
     }
-
     //Free resources and close SDL
     close();
-
     return 0;
 }
